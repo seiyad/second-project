21a08a1ad07ab99480bb026e9cae1c4d7c0f6beb
seiyad <seiyad823@gmail.com>
2026-02-06 13:29:28 +0530

    all little changes

--- pages/expence.js
+++ pages/expence.js
@@ -1,10 +1,11 @@
-function deleteRow(btn) {
-    let row = btn.closest("tr");
-    let name = row.children[0].innerText;
 
-    let catagorie = JSON.parse(localStorage.getItem("catagorie")) || [];
-    catagorie = catagorie.filter(cat => cat.name !== name);
-    localStorage.setItem("catagorie", JSON.stringify(catagorie));
+// function deleteRow(btn) {
+//     let row = btn.closest("tr");
+//     let name = row.children[0].innerText;
 
-    row.remove();
-}
+//     let catagorie = JSON.parse(localStorage.getItem("catagorie")) || [];
+//     catagorie = catagorie.filter(cat => cat.name !== name);
+//     localStorage.setItem("catagorie", JSON.stringify(catagorie));
+
+//     row.remove();
+// }
