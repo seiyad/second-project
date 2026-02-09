8730ab3fa2d04b37b8b9600021573516bc6840f3
seiyad <seiyad823@gmail.com>
2026-02-09 16:30:32 +0530

    sumlitttle changes

--- pages/expence.js
+++ pages/expence.js
@@ -1,11 +1,5 @@
-
-// function deleteRow(btn) {
-//     let row = btn.closest("tr");
-//     let name = row.children[0].innerText;
-
-//     let catagorie = JSON.parse(localStorage.getItem("catagorie")) || [];
-//     catagorie = catagorie.filter(cat => cat.name !== name);
-//     localStorage.setItem("catagorie", JSON.stringify(catagorie));
-
-//     row.remove();
-// }
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+ import { getDatabase,ref,push ,onValue,remove,set } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
+const appSetting = {
+    databaseURL:"https://expense-trakker-default-rtdb.firebaseio.com/"
+};
