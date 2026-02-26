205cddebaa42035f89be4f80167eeb82d5d014fc
seiyad <seiyad823@gmail.com>
2026-02-26 19:09:55 +0530

    dashboard page chnges

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,23 +1,22 @@
 import { auth } from "../firebase.js";
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { onAuthStateChanged, signOut }
+from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
 const logoutBtn = document.getElementById("logout");
 const userEmail = document.getElementById("userEmail");
 
 onAuthStateChanged(auth, (user) => {
+
   if (!user) {
     window.location.href = "../index.html";
   } else {
     userEmail.textContent = user.email;
   }
+
 });
 
-logout.addEventListener("click", () => {
-  signOut(auth)
-    .then(() => {
-      window.location.href = "../index.html";
-    })
-    .catch((error) => {
-      console.error(error);
+logoutButton.addEventListener("click", () => {
+    signOut(auth).then(() => {
+        window.location.href = "../index.html";
     });
 });
