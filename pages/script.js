4f075e3a539aef6d30364cd417c51b854777f1c6
seiyad <seiyad823@gmail.com>
2026-03-05 13:56:32 +0530

    rechange fellings in scriptjs

--- pages/script.js
+++ pages/script.js
@@ -1,6 +1,6 @@
-import{initialaizeApp} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js"
-import{getAuth , signInWithEmailAndPassword,onAuthSyatechanged}
-from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged }
+  from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAWfXbLyYQKrWfnZPdpo25WOr7n9N4M78c",
@@ -10,40 +10,40 @@ const firebaseConfig = {
   messagingSenderId: "654573857096",
   appId: "1:654573857096:web:a07423978542be8a086b7d"
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-const emailInput=document.getElementById('username');
-const passwordInput=document.getElementById('password');
-const loginBtn=document.getElementById('login');
-const registerBtn=document.getElementById('register');
+const emailInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginBtn = document.getElementById('login');
+const registerBtn = document.getElementById('register');
 
-
-onAuthSyatechanged(auth,(user)=>{
-    if (user){
-        window.location.href="./pages/dashborard.html"
-    }
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = "./pages/dashboard.html";
+  }
 });
 
-loginBtn.addEventListener('click',() => {
-    const email =emailInput.value.trim();
-    const password = passwordInput.value.trim();
+loginBtn.addEventListener('click', () => {
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
 
-    if(!email || !password){
-        alert("please fill the valid email and password");
-        return
-    };
+  if (!email || !password) {
+    alert("Please fill in a valid email and password");
+    return;
+  }
 
-    signInWithEmailAndPassword(auth ,email,password)
-        .then(()=>{
-            window.location.href="./page/dashborard.html"
-        })
-        .catch(error);
-            console.error(
-                alert("invalid  email and password")
-            );
-
-    registerBtn.addEventListener('click',()=>{
-        window.location.href="./page/register.html"
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "./pages/dashboard.html";
     })
-})
+    .catch((error) => {
+      console.error(error);
+      alert("Invalid email or password");
+    });
+});
+
+registerBtn.addEventListener('click', () => {
+  window.location.href = "./pages/register.html";
+});
